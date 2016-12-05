f0da23b76bb4d322914bf5fd91e4c98a2a463c66
Evgeny Evseev <pelid80@gmail.com>
2016-12-05 10:16:56 +0300

    minor fix - refines view name

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def form():
     return render_template('form.html')
 
 if __name__ == "__main__":
