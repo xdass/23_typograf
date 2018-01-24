f5ace5f68c993fcca70b89f9423029603aaa7632
Dass <420600@gmail.com>
2018-01-24 23:35:02 +0500

    Init commit

--- server.py
+++ server.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def form():
     return render_template('form.html')
+
+
+@app.route('/action', methods=['POST'])
+def action():
+    print(request.form)
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run()
